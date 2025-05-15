0616be1b0178bd7042a32af908098e76e0d4087f
Ranxin Li <ranxinli2024@gmail.com>
2025-05-15 07:25:22 -0700

    add quantization

---
+++ demo_code/init_openai.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
+openai_api_key=os.getenv("OPENAI_API_KEY")
+openai_client = OpenAI(
+    api_key=openai_api_key
+)
+def query_gpt4(prompt):
+    response = openai_client.chat.completions.create(
+            model="gpt-4",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.7,
+            max_tokens=100
+    )
+    return response.choices[0].message.content.strip()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from demo_code.LLM_Evaluation import evaluate_LLM
+from demo_code.reduce_computational_cost import five_method
 def main():
-    evaluate_LLM()
-    
+    # evaluate_LLM()
+    five_method()
+
 if __name__=='__main__':
     main()
