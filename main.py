6643df2f89a2fa1acb86cc30c9f8a458cd27d424
RanxinLi <rstli@ucdavis.edu>
2025-05-16 05:31:36 -0700

    update distillation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from demo_code.LLM_Evaluation import evaluate_LLM
 from demo_code.reduce_computational_cost import five_method
+from demo_code.output_control import output_control_demo
 def main():
     # evaluate_LLM()
-    five_method()
+    # five_method()
+    output_control_demo()
 
 if __name__=='__main__':
     main()
