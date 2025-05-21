a1a4d053b863d03f1db3cb5caf30c0b6f8353c88
RanxinLi <rstli@ucdavis.edu>
2025-05-21 09:47:51 -0700

    update oov part

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from demo_code.output_control import output_control_demo
 from demo_code.pre_trained_objectives import pre_trained_demo
 from demo_code.context_window_demo import context_window_demo
 from demo_code.fine_tune_demo import fine_tune_demo
+from demo_code.oov_demo import oov_demo
 def main():
     # evaluate_LLM()
     # five_method()
     # output_control_demo()
     # pre_trained_demo()
     # context_window_demo()
-    fine_tune_demo()
+    # fine_tune_demo()
+    oov_demo()
 
 if __name__=='__main__':
     main()
