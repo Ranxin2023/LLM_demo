ac9207842db0e099641f8725fa3fc76ecdfecf7d
Ranxin Li <ranxinli2024@gmail.com>
2025-06-08 22:36:20 -0700

    update output control part

--- demo_code/init_openai.py
+++ demo_code/init_openai.py
@@ -6,10 +6,11 @@ openai_api_key=os.getenv("OPENAI_API_KEY")
 openai_client = OpenAI(
     api_key=openai_api_key
 )
-def query_open_ai(prompt=None, temperature=None, model_name=None, max_tokens=None):
+def query_open_ai(prompt=None, temperature=None, model_name=None, max_tokens=None, top_p=None):
     response = openai_client.chat.completions.create(
             model=model_name,
             messages=[{"role": "user", "content": prompt}],
+            top_p=top_p,
             temperature=temperature,
             max_tokens=max_tokens
     )
--- demo_code/output_control.py
+++ demo_code/output_control.py
@@ -1,22 +1,34 @@
 from contextlib import redirect_stdout
 from transformers import pipeline, set_seed
 from demo_code.init_openai import query_open_ai
-def temperature():
+def temperature_demo():
     print("temperature......")
-    response = query_open_ai( temperature=0.2)
+    model_name="gpt-4"
+    prompt="Write a poem about space"
+    max_tokens=50
+    temperature=0.2
+    response = query_open_ai(model_name=model_name, prompt=prompt, temperature=temperature, max_tokens=max_tokens)
     print("Low Temperature Output:", response)
 def top_k_sampling():
     print("top k sampling......")
     
-    generator = pipeline("text-generation", model="gpt2")
+    generator = pipeline("text-generation", model="gpt2", framework="pt")
     set_seed(42)
 
     output = generator("Once upon a time", max_length=50, top_k=40)
     print("Top-K Sampling Output:", output[0]["generated_text"])
 def top_p_sampling():
-    pass
+    print("top p sampling...")
+    model_name="gpt-4"
+    prompt="Explain quantum computing in simple terms"
+    top_p=0.8
+    temperature=1.0
+    max_tokens=60
+    response=query_open_ai(model_name=model_name, prompt=prompt, temperature=temperature, top_p=top_p, max_tokens=max_tokens)
+    print("Top-P Sampling Output:", response)
 def output_control_demo():
     with open("./output_results/output_control.txt", "w", encoding="utf-8") as f:
         with redirect_stdout(f):
-            temperature()
+            temperature_demo()
             top_k_sampling()
+            top_p_sampling()
