a3b3ef7f3149cf5008d37478e086182871da1d1b
RanxinLi <rstli@ucdavis.edu>
2025-06-11 03:45:55 -0700

    update agent part

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from demo_code.LLM_Evaluation import evaluate_LLM
-from demo_code.reduce_computational_cost import five_method
-from demo_code.output_control import output_control_demo
-from demo_code.pre_trained_objectives import pre_trained_demo
-from demo_code.context_window_demo import context_window_demo
-# from demo_code.fine_tune_demo import fine_tune_demo
-from demo_code.oov_demo import oov_demo
+# from demo_code.LLM_Evaluation import evaluate_LLM
+# from demo_code.reduce_computational_cost import five_method
+# from demo_code.output_control import output_control_demo
+# from demo_code.pre_trained_objectives import pre_trained_demo
+# from demo_code.context_window_demo import context_window_demo
+# # from demo_code.fine_tune_demo import fine_tune_demo
+# from demo_code.oov_demo import oov_demo
 from demo_code.Agent_demo.simple_version import simple_version
 def main():
     # evaluate_LLM()
